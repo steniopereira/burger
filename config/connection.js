659ecf95493ee0bdeff262a4963a59336f7a7b78
Stenio Pereira <steniocp@gmail.com>
2018-05-05 10:23:29 -0700

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@
 var mysql = require("mysql");
 
 var connection;
+
 if(process.env.JAWS_URL){
 connection = mysql.createConnection(process.env.JAWS_URL);
 
 } else {
   connection = mysql.createConnection({
-  port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
