0d17bf37970d88f0a5eb623d70ad2271054d0c53
Stenio Pereira <steniocp@gmail.com>
2018-05-05 10:54:40 -0700

    update

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,7 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
   connection = mysql.createConnection({
+  port: 3306,
   host: "localhost",
   user: "root",
   password: "root",
